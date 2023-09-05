8166dc13d55dafc8ee5da5d50fe2b8064d3a83b5
teresa881016 <teresa881016@gmail.com>
2023-09-05 21:46:47 +0900

    init commit

---
+++ src/main/java/com/example/post_part2/PostPart2Application.java
@@ -0,0 +1,15 @@
+package com.example.post_part2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class PostPart2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostPart2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/post_part2/PostPart2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.post_part2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostPart2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
